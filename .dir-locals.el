55ab3814559c06f2c324b9aa3855309c45f5b627
Tim Hawes <trhawes@gmail.com>
2024-12-30 11:08:42 -0500

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((org-num-skip-unnumbered . t)
+              (org-num-skip-commented . t)
+              (org-num-max-level . 2))))
